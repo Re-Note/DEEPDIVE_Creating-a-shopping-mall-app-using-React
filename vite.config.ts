407be34e3b0d573a01cdfb006b80687ce8edc1e9
Re-Note <dldydtn207@gmail.com>
2024-10-01 13:52:37 +0900

    feat: deplyment 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/DEEPDIVE_Creating-a-shopping-mall-app-using-React/', // GitHub 리포지토리 이름으로 설정
   plugins: [react()],
-})
+});
