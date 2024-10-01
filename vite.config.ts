6399ed98c34cf7492df30432db127111c2b8346c
Re-Note <dldydtn207@gmail.com>
2024-10-01 16:20:09 +0900

    feat: deployment 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/DEEPDIVE_Creating-a-shopping-mall-app-using-React/', // GitHub 리포지토리 이름으로 설정
+  base: '/DEEPDIVE_Creating-a-shopping-mall-app-using-React/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html', // 메인 파일 경로
+        fallback: './404.html', // 404 fallback 파일 경로
+      },
+    },
+  },
+  server: {
+    open: true, // 개발 서버 실행 시 자동으로 브라우저 열기
+  },
 });
